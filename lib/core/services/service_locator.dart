37a948873f88eb8dae118e320016e687c6911401
WafikSleim <wafik_sleim@outlook.com>
2023-08-14 10:28:35 +0300

    version 0.0.1+3

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myfatoorah_api/core/constants.dart';
+import 'package:myfatoorah_api/payment/data/data_sources/payment_remote_data_source.dart';
+import 'package:myfatoorah_api/payment/data/repositories/payment_repository.dart';
 
 final instance = GetIt.instance;
 
@@ -16,6 +18,7 @@ init(String apiToken) {
     ),
   );
 
+  instance.registerLazySingleton(() => PaymentRemoteDataSource(instance<Dio>()));
 
-
+  instance.registerLazySingleton(() => PaymentRepository(instance()));
 }
